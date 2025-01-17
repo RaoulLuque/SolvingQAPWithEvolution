8a309be4b1875f26092581fe598cab61dd49b108
RaoulLuque <raoulsluque@gmail.com>
2025-01-17 11:16:51 +0100

    Add reading in of data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+from src.read_data import read_data
+
 def main():
-    print("Hello World!")
+    flow_matrix, distance_matrix = read_data()
+    print(flow_matrix, distance_matrix)
 
 
 if __name__ == "__main__":
---
+++ src/read_data.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def read_data():
+    data = np.loadtxt('../data/tai256c.dat', skiprows=1)
+    flow_matrix = np.int32(data[:256])
+    distance_matrix = np.int32(data[256:])
+    return flow_matrix, distance_matrix
